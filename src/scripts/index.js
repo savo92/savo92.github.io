0ab5d0e7bf7fa57847bc6b2c69273047f8f7eba8
Lorenzo Savini <lorenzo.savini@nextroll.com>
2021-04-04 11:48:26 +0200

    Starting from scratch

--- src/scripts/index.js
+++
@@ -1,8 +0,0 @@
-import 'semantic-ui-css/semantic.min.css';
-import "../styles/main.scss";
-import { h, render } from "preact";
-
-import App from "./app";
-
-
-render(<App />, document.getElementById("app"));
